62899e7f8f669519fbb7cb966a6f434f54996ac5
mrudul03 <mrudul.palvankar@gmail.com>
2016-10-01 20:58:58 +0530

    added new service

---
+++ accounting-command-service/src/main/java/accounting/AccountingCommandServiceMain.java
@@ -0,0 +1,17 @@
+package accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+@ComponentScan
+@EntityScan("accounting.model")
+public class AccountingCommandServiceMain {
+	public static void main(String[] args) {
+        SpringApplication.run(AccountingCommandServiceMain.class, args);
+    }
+}
